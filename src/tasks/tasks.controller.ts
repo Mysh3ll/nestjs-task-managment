6aaa864779ea36e7b7a04c79ae40913b8e47353a
Michel POMPAS <L_200684026@next.loc>
2020-01-28 16:18:02 +0100

    Add task filter (status/search)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,38 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from "./tasks.service";
 import { Task, TaskStatus } from "./task.model";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private taskService: TasksService) {}
+    constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.taskService.getAllTasks();
+    @UsePipes(ValidationPipe)
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        }
+        return this.tasksService.getAllTasks();
     }
 
     @Get('/:id')
     getTaskById(@Param('id') id: string): Task {
-        return this.taskService.getTaskById(id);
+        return this.tasksService.getTaskById(id);
     }
 
     @Post()
     @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.taskService.createTask(createTaskDto);
+        return this.tasksService.createTask(createTaskDto);
     }
 
     @Patch('/:id/status')
     updateTaskStatusById(
         @Param('id') id: string,
         @Body('status', TaskStatusValidationPipe) status: TaskStatus): Task {
-        return this.taskService.updateTaskStatusById(id, status);
+        return this.tasksService.updateTaskStatusById(id, status);
     }
 
     @Delete('/:id')
     deleteTaskById(@Param('id') id: string): void {
-        this.taskService.deleteTaskById(id);
+        this.tasksService.deleteTaskById(id);
     }
 }
