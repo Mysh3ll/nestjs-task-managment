1f6970451902f7507f3d92936a1f9eb9ab43c333
Michel POMPAS <L_200684026@next.loc>
2020-01-28 21:08:57 +0100

    Refactoring getTaskById

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    Query,
+    UsePipes,
+    ValidationPipe
+} from '@nestjs/common';
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { Task } from "./task.entity";
 
 @Controller('tasks')
 export class TasksController {
@@ -16,12 +29,12 @@ export class TasksController {
     //     }
     //     return this.tasksService.getAllTasks();
     // }
-    //
-    // @Get('/:id')
-    // getTaskById(@Param('id') id: string): Task {
-    //     return this.tasksService.getTaskById(id);
-    // }
-    //
+
+    @Get('/:id')
+    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.tasksService.getTaskById(id);
+    }
+
     // @Post()
     // @UsePipes(ValidationPipe)
     // createTask(@Body() createTaskDto: CreateTaskDto): Task {
