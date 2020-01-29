560912447433a5b051dc8540e05a9afbdc0e6f4f
Michel POMPAS <L_200684026@next.loc>
2020-01-29 15:17:24 +0100

    Add AuthGuard to TaskController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
-import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { GetUser } from "./get-user.decorator";
@@ -19,11 +18,5 @@ export class AuthController {
     @Post('/signin')
     signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
-    }
-
-    @Post('/test')
-    @UseGuards(AuthGuard())
-    test(@GetUser() user: User) {
-        console.log(user);
     }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,10 +7,11 @@ import {
     ParseIntPipe,
     Patch,
     Post,
-    Query,
+    Query, UseGuards,
     UsePipes,
     ValidationPipe
 } from '@nestjs/common';
+import { AuthGuard } from "@nestjs/passport";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe";
@@ -19,6 +20,7 @@ import { Task } from "./task.entity";
 import { TaskStatus } from "./task-status.enum";
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
